4e644a4da7fac1d44c2afca255988770e588a413
Jamie Gorson <jamie.gorson@students.olin.edu>
2015-06-14 10:06:28 -0400

    comibinign preprocessing

--- importingAndPreprocessing.py
+++ importingAndPreprocessing.py
@@ -92,7 +92,7 @@ def convertImagesToGrayscale(colorImages, show=False):
     cv2.destroyAllWindows()
     return grayscaleImages
 
-def detectEdges(img, i):
+def detectEdges(img, i, show=False):
     """Uses canny to detect edges
         inputs: img - already uploaded img, i - integer
         output: edges of image"""
@@ -103,19 +103,25 @@ def detectEdges(img, i):
     edges = cv2.Canny(img, threshold1, threshold2, sobel, L2gradient=gradient)
     canny_result = np.copy(img)
     canny_result[edges.astype(np.bool)]=0
-    cv2.imshow('cannyresult'+str(i),cv2.resize(edges,(0,0), fx=.25, fy=.25))
-    cv2.waitKey(0)
+    if show:
+        cv2.imshow('cannyresult'+str(i),cv2.resize(edges,(0,0), fx=.25, fy=.25))
+        cv2.waitKey(0)
     return edges
 
-
+def allDetectEdges(imgs):
+    allEdges=[]
+    for i in range(len(imgs)):
+        edges = detectEdges(imgs[i],i)
+        allEdges.append(edges)  
+    return allEdges  
     
 def process_image(img, kernel=13):
     """runs to process for imputting one image"""
     #gradients is a tuple of two images: xgrad and ygaad
     imgEdit = preprocess_image(img, kernel = kernel)
-    gradients=calculateXYGradient(imgEdit, show=True)
+    gradients=calculateXYGradient(imgEdit, show=False)
     return gradients
-def preprocessImagesGradient(images):
+def preprocessImagesGradient(images, kernel=13):
     xgrads = []
     ygrads = []
     for i in range(len(images)):
@@ -154,9 +160,44 @@ def calculateXYGradient(img, show=False):
         cv2.waitKey(0)
         """cv2.imshow('xyGradient',cv2.resize(sobelxy, (0,0), fx=0.25, fy=0.25))
         cv2.waitKey(0)"""
+    #print sobelx
     return sobelx, sobely
+def combineXYGradient(xgrads, ygrads, show=False):
+    allXYgradients = []
+    for i in range(len(xgrads)):
+        convertedx = 255-xgrads[i]
+        convertedy = 255-ygrads[i]
+        together = np.multiply(convertedx,convertedy)
+        together *= 255.0/together.max()   
+        readjust = 255-together
+        if show:
+            cv2.imshow('x&ygradient', cv2.resize(readjust, (0,0), fx=0.25, fy=0.25))
+            cv2.waitKey(0)
+        allXYgradients.append(together)
+    return allXYgradients
 
+def combine2Images(img1, img2, show=False):
+        converted1 = 255-img1
+        converted2 = 255-img2
+        together = np.multiply(converted1,converted2)
+        together *= 255.0/together.max()   
+        readjust = 255-together
+        if show:
+            cv2.imshow('imgs together', cv2.resize(readjust, (0,0), fx=0.25, fy=0.25))
+            cv2.waitKey(0)
+
+def combineAll(imgs1, imgs2, show=False):
+    for i in range(len(imgs1)):
+        combine2Images(imgs1[i],imgs2[i], show=show)
 
 if __name__ == '__main__':
-    out = import_images('_Data/Radiographs')
-    #preprocess_all_images(out)
+    images = import_images('_Data/Radiographs')
+    processed=preprocess_all_images(images, show=False)
+    edges = allDetectEdges(images)
+    combineAll(processed,edges, show=True)
+    
+    
+    #xgradients, ygradients = preprocessImagesGradient(images)
+    #
+    #combineXYGradient(xgradients,ygradients, show=True)
+    
